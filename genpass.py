b630f21c028425b56e0b4663025b1335717e608e
Ever Leiva <67029457+ever186@users.noreply.github.com>
2025-09-06 12:33:45 -0500

    genpass.py
    sin limites

--- genpass.py
+++ genpass.py
@@ -12,11 +12,10 @@ ascii_art = f.renderText('GENPASS')
 print(Fore.BLUE + ascii_art + Fore.WHITE + "         [*] Creado por hack4ever " + "\n")
 
 def main():
-
-    print(Fore.GREEN + Style.BRIGHT + "[+] Bienvenido a la aplicación de generacion de contraseñas")
+    print(Fore.GREEN + Style.BRIGHT + "[+] Bienvenido a la aplicación de generación de contraseñas")
     print(Fore.YELLOW + Style.BRIGHT + "1. Generar contraseña aleatoria")
-    print(Fore.YELLOW + Style.BRIGHT + "2. Generar contraseña por numero de telefono")
-    print(Fore.YELLOW + Style.BRIGHT + "3. generar contraseñas personlizadas")
+    print(Fore.YELLOW + Style.BRIGHT + "2. Generar contraseña por número de teléfono")
+    print(Fore.YELLOW + Style.BRIGHT + "3. Generar contraseñas personalizadas")
     print(Fore.YELLOW + Style.BRIGHT + "4. Salir")
     contraseña = int(input(Fore.BLUE + Style.BRIGHT + "[+] Ingrese una opción: "))
     select(contraseña)
@@ -31,7 +30,7 @@ def select(contraseña):
     elif contraseña == 4:
         exit()
     else:
-        print(Fore.RED + Style.BRIGHT + "[+] Opción no valida")
+        print(Fore.RED + Style.BRIGHT + "[+] Opción no válida")
         main()
 
 def aleatoria():
@@ -39,13 +38,13 @@ def aleatoria():
 
     f = pyfiglet.Figlet(font='slant')
     ascii_art = f.renderText('GENPASS')
-    print( Fore.BLUE + ascii_art)
+    print(Fore.BLUE + ascii_art)
 
     cantidad = int(input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese la cantidad de contraseñas: "))
-    print(Fore.GREEN + Style.BRIGHT + "[+] Generando contraseña aleatoria", end="\n")
+    print(Fore.GREEN + Style.BRIGHT + "[+] Generando contraseñas aleatorias...\n")
 
     nombres = ["Alejandro", "Carlos", "Diego", "Fernando", "Guillermo", "Hugo", "Luis", "Manuel", 
-               "Pedro", "Rafael", "Santiago", "Tomas", "Victor", "javier", "Maria", "Juan", 
+               "Pedro", "Rafael", "Santiago", "Tomas", "Victor", "Javier", "Maria", "Juan", 
                "Miguel", "Valery", "Maira"]
 
     apellidos = ["Alvarez", "Arias", "Benitez", "Castro", "Diaz", "Espinosa", "Garcia", 
@@ -56,9 +55,10 @@ def aleatoria():
 
     signos = ["+", "-", "*", "/", "%", "!", "?", "&", "@", "#", "$", "|", ";", ".", ",", "_", "="]
 
+    total_generadas = 0
+
     with open("aleatoria.txt", "w") as f:
         for i in range(cantidad):
-            i = i + 1
             nombre = choice(nombres)
             apellido = choice(apellidos)
             palabra = choice(palabras)
@@ -69,22 +69,24 @@ def aleatoria():
             f.write(palabra + str(randint(1000, 9999)) + "\n")
             f.write(palabra + signo + str(randint(1000, 9999)) + "\n")
 
-            print(f"palabra {i} generada", end="\r")
+            total_generadas += 4
+            print(f"Palabra {total_generadas} generada")
 
-        print(Fore.GREEN + Style.BRIGHT + "[+] Contraseñas guardadas en aleatoria.txt")
-        main()
+    print(Fore.GREEN + Style.BRIGHT + f"\n[+] Contraseñas guardadas en aleatoria.txt")
+    print(Fore.CYAN + f"[+] Total de contraseñas generadas: {total_generadas}")
+    main()
+
 def telefono():
     os.system('cls')
 
     f = pyfiglet.Figlet(font='slant')
     ascii_art = f.renderText('GENPASS')
-    print( Fore.BLUE + ascii_art)
+    print(Fore.BLUE + ascii_art)
 
     cantidad = int(input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese la cantidad de contraseñas: "))
-    print(Fore.YELLOW + Style.BRIGHT + "[+] Generando contraseña por numero de telefono", end="\n")
+    print(Fore.YELLOW + Style.BRIGHT + "[+] Generando contraseña por número de teléfono", end="\n")
     with open("telefono.txt", "w") as f:
-        for i in range(10):
-            i = i + 1
+        for i in range(cantidad):
             f.write("300" +str(randint(2000000, 9399999)) + "\n")
             f.write("301" +str(randint(2000000, 7999999)) + "\n")
             f.write("302" +str(randint(2000000, 4699999)) + "\n")
@@ -107,26 +109,25 @@ def telefono():
             f.write("323" +str(randint(2000000, 9999999)) + "\n")
             f.write("324" +str(randint(1000000, 9999999)) + "\n")
 
-            print(f"numero {i} generada", end="\r")
-        print(Fore.GREEN + Style.BRIGHT + "[+] Contraseñas guardadas en telefono.txt")
-    main()
+            print(f"Número {i+1} generado")
 
+    print(Fore.GREEN + Style.BRIGHT + "[+] Contraseñas guardadas en telefono.txt")
+    main()
 
 def personalizada():
     os.system('cls')
 
     f = pyfiglet.Figlet(font='slant')
     ascii_art = f.renderText('GENPASS')
-    print( Fore.BLUE + ascii_art)
+    print(Fore.BLUE + ascii_art)
 
     cantidad = int(input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese la cantidad de contraseñas: "))
 
     user = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese el nombre de la persona: ")
     lastname = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese el apellido: ")
     fecha = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese la fecha de nacimiento[ddmmaa]: ")
-    user_num = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese el numero de telefono: ")
+    user_num = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese el número de teléfono: ")
     apodo = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese el apodo: ")
-
 
     user_mon = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese el nombre de la madre: ")
     lastname_mon = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese el apellido de la madre: ")
@@ -140,71 +141,67 @@ def personalizada():
     fechas = [fecha, fecha_mon, fecha_father, ""]
     numeros = [user_num]
 
-    par = input(Fore.GREEN + Style.BRIGHT + "[+] tiene pareja[s/n]: ")
+    par = input(Fore.GREEN + Style.BRIGHT + "[+] ¿Tiene pareja [s/n]?: ")
     if par == "s":
         pareja = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese el nombre de la pareja: ")
         lastname_par = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese el apellido de la pareja: ")
-        num_par = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese el numero de telefono de la pareja: ")
+        num_par = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese el número de teléfono de la pareja: ")
         fecha_par = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese la fecha de nacimiento de la pareja [ddmmaa]: ")
         nombres.append(pareja)
         apellidos.append(lastname_par)
         numeros.append(num_par)
         fechas.append(fecha_par)
-    else:
-        pass
 
-    herm = input(Fore.GREEN + Style.BRIGHT + "[+] tiene hermanos[s/n]: ")
+    herm = input(Fore.GREEN + Style.BRIGHT + "[+] ¿Tiene hermanos [s/n]?: ")
     if herm == "s":
-        hermano_1 = input(Fore.GREEN + Style.BRIGHT + "[+] Ingresa nombre de hermano: ")
-        lastname_her_1 = input(Fore.GREEN + Style.BRIGHT + "[+] Ingresa apellido de hermano: ")
-        hermano_2 = input(Fore.GREEN + Style.BRIGHT + "[+] Ingresa nombre de hermano: ")
-        lastname_her_2 = input(Fore.GREEN + Style.BRIGHT + "[+] Ingresa apellido de hermano: ")
-        nombres.append(hermano_1)
-        apellidos.append(lastname_her_1)
-        nombres.append(hermano_2)
-        apellidos.append(lastname_her_2)
-    else:
-        pass
+        hermano_1 = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese nombre de hermano: ")
+        lastname_her_1 = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese apellido de hermano: ")
+        hermano_2 = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese nombre de hermano: ")
+        lastname_her_2 = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese apellido de hermano: ")
+        nombres.extend([hermano_1, hermano_2])
+        apellidos.extend([lastname_her_1, lastname_her_2])
 
-    hijo = input(Fore.GREEN + Style.BRIGHT + "[+] tiene hijos[s/n]: ")
+    hijo = input(Fore.GREEN + Style.BRIGHT + "[+] ¿Tiene hijos [s/n]?: ")
     if hijo == "s":
-        user_child = input(Fore.GREEN + Style.BRIGHT + "[+] Ingresa nombre de hijo: ")
-        lastname_child = input(Fore.GREEN + Style.BRIGHT + "[+] Ingresa apellido de hijo: ")
-        fecha_child = input(Fore.GREEN + Style.BRIGHT + "[+] Ingresa fecha de nacimiento de hijo [ddmmaa]: ")
+        user_child = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese nombre de hijo: ")
+        lastname_child = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese apellido de hijo: ")
+        fecha_child = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese fecha de nacimiento de hijo [ddmmaa]: ")
         nombres.append(user_child)
         apellidos.append(lastname_child)
         fechas.append(fecha_child)
-    else:
-        pass
-    mascota = input(Fore.GREEN + Style.BRIGHT + "[+] tiene mascota[s/n]: ")
+
+    mascota = input(Fore.GREEN + Style.BRIGHT + "[+] ¿Tiene mascota [s/n]?: ")
     if mascota == "s":
-        mascota = input(Fore.GREEN + Style.BRIGHT + "[+] Ingresa nombre de mascota: ")
-        fecha_mascota = input(Fore.GREEN + Style.BRIGHT + "[+] Ingresa fecha de adoptacion de mascota [ddmmaa]: ")
+        mascota = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese nombre de mascota: ")
+        fecha_mascota = input(Fore.GREEN + Style.BRIGHT + "[+] Ingrese fecha de adopción de mascota [ddmmaa]: ")
         nombres.append(mascota)
         fechas.append(fecha_mascota)
-    else:    
-        pass
 
     signos = ["","+", "-", "*", "%", "!", "&", "@", "#", "$", ".", "_"]
 
-    print(Fore.YELLOW + Style.BRIGHT + "[+] Generando contraseña personalizada", end="\n")
+    print(Fore.YELLOW + Style.BRIGHT + "[+] Generando contraseña personalizada...\n")
+    total_generadas = 0
     with open("personalizada.txt", "w") as f:
         for i in range(cantidad):
             nombre = choice(nombres)
             apellido = choice(apellidos)
             fecha = choice(fechas)
             signo = choice(signos)
-            numeros = choice(numeros)
-
-            i = i + 1
+            numeros_sel = choice(numeros)
 
             f.write(nombre + apellido + fecha + "\n")
             f.write(apodo + signo + fecha +"\n")
             f.write(nombre + signo + fecha + "\n")
-            f.write(numeros + signo + apodo + "\n")
-            f.write(numeros + signo + nombre + "\n")
-    
-        print(Fore.GREEN + Style.BRIGHT + "[+] Contraseñas guardadas en personalizada.txt")
-        main()
+            f.write(numeros_sel + signo + apodo + "\n")
+            f.write(numeros_sel + signo + nombre + "\n")
+
+            total_generadas += 5
+            print(f"Contraseña {total_generadas} generada")
+
+    print(Fore.GREEN + Style.BRIGHT + f"\n[+] Contraseñas guardadas en personalizada.txt")
+    print(Fore.CYAN + f"[+] Total de contraseñas generadas: {total_generadas}")
+    main()
 
 main()
+
+
